645352d7cbe6f4efa2663a0282d826ae442beb91
Annie <annie@nylas.com>
2016-06-30 17:42:42 -0700

    fixed linting

--- internal_packages/composer-signature/spec/signature-composer-dropdown-spec.jsx
+++ internal_packages/composer-signature/spec/signature-composer-dropdown-spec.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-// curly braces are destructuring syntax
-import SignatureComposerDropdown from '../lib/signature-composer-dropdown'
-import {renderIntoDocument} from '../../../spec/nylas-test-utils'
-import ReactTestUtils from 'react-addons-test-utils'
-import {findDOMNode} from 'react-dom';
-import Contenteditable from '../../../src/components/contenteditable/contenteditable';
-
-// adds default sig into composer
-  // sig is selected
-  // on edit - sig is not selected
-// when change from field
-  // if from field has different default sig, it changes
-describe('SignatureComposerDropdown', function signatureComposerDropdown() {
-
-})
+// import React from 'react';
+// // curly braces are destructuring syntax
+// import SignatureComposerDropdown from '../lib/signature-composer-dropdown'
+// import {renderIntoDocument} from '../../../spec/nylas-test-utils'
+// import ReactTestUtils from 'react-addons-test-utils'
+// import {findDOMNode} from 'react-dom';
+// import Contenteditable from '../../../src/components/contenteditable/contenteditable';
+//
+// // adds default sig into composer
+//   // sig is selected
+//   // on edit - sig is not selected
+// // when change from field
+//   // if from field has different default sig, it changes
+// describe('SignatureComposerDropdown', function signatureComposerDropdown() {
+//
+// })
